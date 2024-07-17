85849464ab1b3ce50540f240276b7b31104caeba
dlwls5201 <dlwls5201@naver.com>
2024-07-17 09:20:34 +0900

    add explicit animation feature

--- lib/screens/explicit_animations_screens.dart
+++ lib/screens/explicit_animations_screens.dart
@@ -7,13 +7,113 @@ class ExplicitAnimationsScreen extends StatefulWidget {
   State<ExplicitAnimationsScreen> createState() => _ExplicitAnimationsScreenState();
 }
 
-class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen> {
+class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 2),
+  );
+
+  /*late final Animation<Color?> _color = ColorTween(
+    begin: Colors.amber,
+    end: Colors.red,
+  ).animate(_animationController);*/
+
+  late final Animation<Decoration> _decoration = DecorationTween(
+    begin: BoxDecoration(
+      color: Colors.amber,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    end: BoxDecoration(
+      color: Colors.red,
+      borderRadius: BorderRadius.circular(120),
+    ),
+  ).animate(_curve);
+
+  late final Animation<double> _rotation =
+      Tween(begin: 0.0, end: 0.5).animate(_curve);
+
+  late final Animation<double> _scale = Tween(begin: 1.0, end: 1.1).animate(_curve);
+
+  late final CurvedAnimation _curve = CurvedAnimation(
+    parent: _animationController,
+    curve: Curves.elasticInOut,
+  );
+
+  void _play() {
+    _animationController.forward();
+  }
+
+  void _pause() {
+    _animationController.stop();
+  }
+
+  void _rewind() {
+    _animationController.reverse();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Explicit Animations"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            /*AnimatedBuilder(
+              animation: _color,
+              builder: (context, child) {
+                return Container(
+                  color: _color.value,
+                  width: 400,
+                  height: 400,
+                );
+              },
+            ),*/
+            ScaleTransition(
+              scale: _scale,
+              child: RotationTransition(
+                turns: _rotation,
+                child: DecoratedBoxTransition(
+                  decoration: _decoration,
+                  child: const SizedBox(
+                    width: 400,
+                    height: 400,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _play,
+                  child: const Text("Play"),
+                ),
+                ElevatedButton(
+                  onPressed: _pause,
+                  child: const Text("Pause"),
+                ),
+                ElevatedButton(
+                  onPressed: _rewind,
+                  child: const Text("Rewind"),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
