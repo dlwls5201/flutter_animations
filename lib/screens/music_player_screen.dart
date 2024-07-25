1123b5f230a11783656293271135b461dd7d3cd8
dlwls5201 <dlwls5201@naver.com>
2024-07-25 09:34:35 +0900

    add music player detail screen

--- lib/screens/music_player_screen.dart
+++ lib/screens/music_player_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_animations_masterclass/screens/music_player_detail_screen.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
   const MusicPlayerScreen({super.key});
@@ -8,8 +11,150 @@ class MusicPlayerScreen extends StatefulWidget {
 }
 
 class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
+  final PageController _pageController = PageController(
+    viewportFraction: 0.8,
+  );
+
+  int _currentPage = 0;
+
+  final ValueNotifier<double> _scroll = ValueNotifier(0.0);
+
+  void _onPageChanged(int newPage) {
+    setState(() {
+      _currentPage = newPage;
+    });
+  }
+
+  void _onTap(int imageIndex) {
+    Navigator.of(context).push(
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return FadeTransition(
+            opacity: animation,
+            child: MusicPlayerDetailScreen(
+              index: imageIndex,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      final page = _pageController.page;
+      if (page == null) return;
+      _scroll.value = page;
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 500),
+            child: Container(
+              key: ValueKey(_currentPage),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/covers/${_currentPage + 1}.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 20,
+                  sigmaY: 20,
+                ),
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ),
+          PageView.builder(
+            onPageChanged: _onPageChanged,
+            controller: _pageController,
+            itemCount: 5,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (
+              context,
+              index,
+            ) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () => _onTap(index + 1),
+                    child: Hero(
+                      tag: "${index + 1}",
+                      child: ValueListenableBuilder(
+                        valueListenable: _scroll,
+                        builder: (context, scroll, child) {
+                          final difference = (scroll - index).abs();
+                          final scale = 1 - (difference * 0.1);
+                          return Transform.scale(
+                            scale: scale,
+                            child: Container(
+                              height: 350,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.4),
+                                    blurRadius: 10,
+                                    spreadRadius: 2,
+                                    offset: const Offset(0, 8),
+                                  )
+                                ],
+                                image: DecorationImage(
+                                  image: AssetImage("assets/covers/${index + 1}.jpg"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    'Interstellar',
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    'Hans Zimmer',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              );
+            },
+          )
+        ],
+      ),
+    );
   }
 }
