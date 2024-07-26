b2684e9b73d4d959bdabed951681ba3fff407be5
dlwls5201 <dlwls5201@naver.com>
2024-07-26 17:25:28 +0900

    add detail screen animation

--- lib/screens/music_player_detail_screen.dart
+++ lib/screens/music_player_detail_screen.dart
@@ -9,12 +9,81 @@ class MusicPlayerDetailScreen extends StatefulWidget {
   State<MusicPlayerDetailScreen> createState() => _MusicPlayerDetailScreenState();
 }
 
-class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen> {
+class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _progressController = AnimationController(
+    vsync: this,
+    duration: const Duration(minutes: 1),
+  )..repeat(reverse: true);
+
+  late final AnimationController _marqueeController = AnimationController(
+    vsync: this,
+    duration: const Duration(
+      seconds: 20,
+    ),
+  )..repeat(reverse: true);
+
+  late final AnimationController _playPauseController = AnimationController(
+    vsync: this,
+    duration: const Duration(
+      milliseconds: 500,
+    ),
+  );
+
+  late final Animation<Offset> _marqueeTween = Tween(
+    begin: const Offset(0.1, 0),
+    end: const Offset(-0.6, 0),
+  ).animate(_marqueeController);
+
+  @override
+  void dispose() {
+    _progressController.dispose();
+    _marqueeController.dispose();
+    super.dispose();
+  }
+
+  void _onPlayPauseTap() {
+    if (_playPauseController.isCompleted) {
+      _playPauseController.reverse();
+    } else {
+      _playPauseController.forward();
+    }
+  }
+
+  bool _dragging = false;
+
+  void _toggleDragging() {
+    setState(() {
+      _dragging = !_dragging;
+    });
+  }
+
+  final ValueNotifier<double> _volume = ValueNotifier(0);
+
+  late final size = MediaQuery.of(context).size;
+
+  void _onVolumeDragUpdate(DragUpdateDetails details) {
+    _volume.value += details.delta.dx;
+    _volume.value = _volume.value.clamp(
+      0.0,
+      size.width - 80,
+    );
+  }
+
+  void _openMenu() {}
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Interstellar"),
+        actions: [
+          IconButton(
+            onPressed: _openMenu,
+            icon: const Icon(Icons.menu),
+          ),
+        ],
       ),
       body: Column(
         children: [
@@ -46,8 +115,204 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen> {
               ),
             ),
           ),
+          const SizedBox(
+            height: 50,
+          ),
+          AnimatedBuilder(
+            animation: _progressController,
+            builder: (context, child) => CustomPaint(
+              size: Size(size.width - 80, 5),
+              painter: ProgressBar(
+                progressValue: _progressController.value,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Row(
+              children: [
+                Text(
+                  "00:00",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  "01:00",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w600,
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Interstellar",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          SlideTransition(
+            position: _marqueeTween,
+            child: const Text(
+              "A Film By Christopher Nolan - Original Motion Picture Soundtrack",
+              maxLines: 1,
+              overflow: TextOverflow.visible,
+              softWrap: false,
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          /*GestureDetector(
+            onTap: _onPlayPauseTap,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AnimatedIcon(
+                  icon: AnimatedIcons.pause_play,
+                  progress: _playPauseController,
+                  size: 60,
+                ),
+              ],
+            ),
+          )*/
+          GestureDetector(
+            onHorizontalDragUpdate: _onVolumeDragUpdate,
+            onHorizontalDragStart: (_) => _toggleDragging(),
+            onHorizontalDragEnd: (_) => _toggleDragging(),
+            child: AnimatedScale(
+              scale: _dragging ? 1.1 : 1,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.bounceOut,
+              child: Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ValueListenableBuilder(
+                  valueListenable: _volume,
+                  builder: (context, value, child) => CustomPaint(
+                    size: Size(size.width - 80, 50),
+                    painter: VolumePainter(
+                      volume: value,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
+  }
+}
+
+class ProgressBar extends CustomPainter {
+  final double progressValue;
+
+  ProgressBar({
+    required this.progressValue,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final progress = size.width * progressValue;
+
+    // track
+    final trackPaint = Paint()
+      ..color = Colors.grey.shade300
+      ..style = PaintingStyle.fill;
+
+    final trackRRect = RRect.fromLTRBR(
+      0,
+      0,
+      size.width,
+      size.height,
+      const Radius.circular(10),
+    );
+
+    canvas.drawRRect(trackRRect, trackPaint);
+
+    // progress
+    final progressPaint = Paint()
+      ..color = Colors.grey.shade500
+      ..style = PaintingStyle.fill;
+
+    final progressRRect = RRect.fromLTRBR(
+      0,
+      0,
+      progress,
+      size.height,
+      const Radius.circular(10),
+    );
+
+    canvas.drawRRect(progressRRect, progressPaint);
+
+    // thumb
+    canvas.drawCircle(
+      Offset(progress, size.height / 2),
+      10,
+      progressPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant ProgressBar oldDelegate) {
+    return oldDelegate.progressValue != progressValue;
+  }
+}
+
+class VolumePainter extends CustomPainter {
+  final double volume;
+
+  VolumePainter({
+    required this.volume,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final bgPaint = Paint()..color = Colors.grey.shade300;
+
+    final bgRect = Rect.fromLTWH(
+      0,
+      0,
+      size.width,
+      size.height,
+    );
+
+    canvas.drawRect(bgRect, bgPaint);
+
+    final volumePaint = Paint()..color = Colors.grey.shade500;
+
+    final volumeRect = Rect.fromLTWH(
+      0,
+      0,
+      volume,
+      size.height,
+    );
+
+    canvas.drawRect(volumeRect, volumePaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant VolumePainter oldDelegate) {
+    return oldDelegate.volume != volume;
   }
 }
