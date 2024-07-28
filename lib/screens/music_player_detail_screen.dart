66c1ff1e218bfb25326bc7ee3dba80873abdbbfd
dlwls5201 <dlwls5201@naver.com>
2024-07-28 11:04:15 +0900

    add menu animation

--- lib/screens/music_player_detail_screen.dart
+++ lib/screens/music_player_detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MusicPlayerDetailScreen extends StatefulWidget {
@@ -23,6 +24,11 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
     ),
   )..repeat(reverse: true);
 
+  late final Animation<Offset> _marqueeTween = Tween(
+    begin: const Offset(0.1, 0),
+    end: const Offset(-0.6, 0),
+  ).animate(_marqueeController);
+
   late final AnimationController _playPauseController = AnimationController(
     vsync: this,
     duration: const Duration(
@@ -30,15 +36,95 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
     ),
   );
 
-  late final Animation<Offset> _marqueeTween = Tween(
-    begin: const Offset(0.1, 0),
-    end: const Offset(-0.6, 0),
-  ).animate(_marqueeController);
+  late final AnimationController _menuController = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 3),
+    reverseDuration: const Duration(seconds: 1),
+  );
+
+  final Curve _menuCurve = Curves.easeInOutCubic;
+
+  late final Animation<double> _screenScale = Tween(
+    begin: 1.0,
+    end: 0.7,
+  ).animate(
+    CurvedAnimation(
+      parent: _menuController,
+      curve: Interval(
+        0.0,
+        0.3,
+        curve: _menuCurve,
+      ),
+    ),
+  );
+
+  late final Animation<Offset> _screenOffset = Tween(
+    begin: Offset.zero,
+    end: const Offset(
+      0.5,
+      0,
+    ),
+  ).animate(
+    CurvedAnimation(
+      parent: _menuController,
+      curve: Interval(
+        0.2,
+        0.4,
+        curve: _menuCurve,
+      ),
+    ),
+  );
+
+  late final Animation<double> _closeButtonOpacity = Tween<double>(
+    begin: 0.0,
+    end: 1.0,
+  ).animate(
+    CurvedAnimation(
+      parent: _menuController,
+      curve: Interval(
+        0.3,
+        0.5,
+        curve: _menuCurve,
+      ),
+    ),
+  );
+
+  late final List<Animation<Offset>> _menuAnimations = [
+    for (var i = 0; i < _menus.length; i++)
+      Tween<Offset>(
+        begin: const Offset(-1, 0),
+        end: Offset.zero,
+      ).animate(
+        CurvedAnimation(
+          parent: _menuController,
+          curve: Interval(
+            0.4 + (0.1 * 1),
+            0.7 + (0.1 * i),
+            curve: _menuCurve,
+          ),
+        ),
+      ),
+  ];
+
+  late final Animation<Offset> _logOutSlide = Tween<Offset>(
+    begin: const Offset(-1, 0),
+    end: Offset.zero,
+  ).animate(
+    CurvedAnimation(
+      parent: _menuController,
+      curve: Interval(
+        0.8,
+        1.0,
+        curve: _menuCurve,
+      ),
+    ),
+  );
 
   @override
   void dispose() {
     _progressController.dispose();
     _marqueeController.dispose();
+    _menuController.dispose();
     super.dispose();
   }
 
@@ -70,156 +156,267 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
     );
   }
 
-  void _openMenu() {}
+  void _openMenu() {
+    _menuController.forward();
+  }
+
+  void _closeMenu() {
+    _menuController.reverse();
+  }
+
+  final List<Map<String, dynamic>> _menus = [
+    {
+      "icon": Icons.person,
+      "text": "Profile",
+    },
+    {
+      "icon": Icons.notifications,
+      "text": "Notifications",
+    },
+    {
+      "icon": Icons.settings,
+      "text": "Settings",
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Interstellar"),
-        actions: [
-          IconButton(
-            onPressed: _openMenu,
-            icon: const Icon(Icons.menu),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 30,
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Hero(
-              tag: "${widget.index}",
-              child: Container(
-                width: 350,
-                height: 350,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.4),
-                      blurRadius: 10,
-                      spreadRadius: 2,
-                      offset: const Offset(0, 8),
-                    )
-                  ],
-                  image: DecorationImage(
-                    image: AssetImage("assets/covers/${widget.index}.jpg"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+            leading: FadeTransition(
+              opacity: _closeButtonOpacity,
+              child: IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: _closeMenu,
               ),
             ),
           ),
-          const SizedBox(
-            height: 50,
-          ),
-          AnimatedBuilder(
-            animation: _progressController,
-            builder: (context, child) => CustomPaint(
-              size: Size(size.width - 80, 5),
-              painter: ProgressBar(
-                progressValue: _progressController.value,
-              ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40),
-            child: Row(
+            child: Column(
               children: [
-                Text(
-                  "00:00",
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.grey,
-                    fontWeight: FontWeight.w600,
+                const SizedBox(
+                  height: 30,
+                ),
+                for (var i = 0; i < _menus.length; i++) ...[
+                  SlideTransition(
+                    position: _menuAnimations[i],
+                    child: Row(
+                      children: [
+                        Icon(
+                          _menus[i]["icon"],
+                          color: Colors.grey.shade200,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          _menus[i]["text"],
+                          style: TextStyle(
+                            color: Colors.grey.shade200,
+                            fontSize: 18,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                ],
+                const Spacer(),
+                SlideTransition(
+                  position: _logOutSlide,
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Icons.logout,
+                        color: Colors.red,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Log out",
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 18,
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                Spacer(),
-                Text(
-                  "01:00",
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.grey,
-                    fontWeight: FontWeight.w600,
-                  ),
+                const SizedBox(
+                  height: 100,
                 )
               ],
             ),
           ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            "Interstellar",
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          SlideTransition(
-            position: _marqueeTween,
-            child: const Text(
-              "A Film By Christopher Nolan - Original Motion Picture Soundtrack",
-              maxLines: 1,
-              overflow: TextOverflow.visible,
-              softWrap: false,
-              style: TextStyle(fontSize: 18),
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          /*GestureDetector(
-            onTap: _onPlayPauseTap,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AnimatedIcon(
-                  icon: AnimatedIcons.pause_play,
-                  progress: _playPauseController,
-                  size: 60,
-                ),
-              ],
-            ),
-          )*/
-          GestureDetector(
-            onHorizontalDragUpdate: _onVolumeDragUpdate,
-            onHorizontalDragStart: (_) => _toggleDragging(),
-            onHorizontalDragEnd: (_) => _toggleDragging(),
-            child: AnimatedScale(
-              scale: _dragging ? 1.1 : 1,
-              duration: const Duration(milliseconds: 500),
-              curve: Curves.bounceOut,
-              child: Container(
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: ValueListenableBuilder(
-                  valueListenable: _volume,
-                  builder: (context, value, child) => CustomPaint(
-                    size: Size(size.width - 80, 50),
-                    painter: VolumePainter(
-                      volume: value,
+        ),
+        SlideTransition(
+          position: _screenOffset,
+          child: ScaleTransition(
+            scale: _screenScale,
+            child: Scaffold(
+              appBar: AppBar(
+                title: const Text("Interstellar"),
+                actions: [
+                  IconButton(
+                    onPressed: _openMenu,
+                    icon: const Icon(Icons.menu),
+                  ),
+                ],
+              ),
+              body: Column(
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Hero(
+                      tag: "${widget.index}",
+                      child: Container(
+                        width: 300,
+                        height: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.4),
+                              blurRadius: 10,
+                              spreadRadius: 2,
+                              offset: const Offset(0, 8),
+                            )
+                          ],
+                          image: DecorationImage(
+                            image: AssetImage("assets/covers/${widget.index}.jpg"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  AnimatedBuilder(
+                    animation: _progressController,
+                    builder: (context, child) => CustomPaint(
+                      size: Size(size.width - 80, 5),
+                      painter: ProgressBar(
+                        progressValue: _progressController.value,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Row(
+                      children: [
+                        Text(
+                          "00:00",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          "01:00",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    "Interstellar",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  SlideTransition(
+                    position: _marqueeTween,
+                    child: const Text(
+                      "A Film By Christopher Nolan - Original Motion Picture Soundtrack",
+                      maxLines: 1,
+                      overflow: TextOverflow.visible,
+                      softWrap: false,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  GestureDetector(
+                    onTap: _onPlayPauseTap,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        AnimatedIcon(
+                          icon: AnimatedIcons.pause_play,
+                          progress: _playPauseController,
+                          size: 60,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  GestureDetector(
+                    onHorizontalDragUpdate: _onVolumeDragUpdate,
+                    onHorizontalDragStart: (_) => _toggleDragging(),
+                    onHorizontalDragEnd: (_) => _toggleDragging(),
+                    child: AnimatedScale(
+                      scale: _dragging ? 1.1 : 1,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.bounceOut,
+                      child: Container(
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: ValueListenableBuilder(
+                          valueListenable: _volume,
+                          builder: (context, value, child) => CustomPaint(
+                            size: Size(size.width - 80, 50),
+                            painter: VolumePainter(
+                              volume: value,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
